c474b833c9fe8c95ecf558c7bfb273f221cdedeb
Bhootnath051 <girirahul6204@gmail.com>
2024-12-18 16:44:53 +0530

    ACO update

---
+++ TMS/ACO/TDSRejectedCases.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Web.UI.WebControls;
+
+public partial class ACO_TDSRejectedCases : System.Web.UI.Page
+{
+    private string strMessage;
+    private SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["MyDbConn"].ConnectionString);
+    private DataTable dt = new DataTable();
+    private DataSet ds = new DataSet();
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!IsPostBack)
+        {
+            LoadHospitals();
+        }
+    }
+    private void LoadHospitals()
+    {
+        try
+        {
+            //using (SqlCommand cmd = new SqlCommand("sp_GetAllHospitalsFromExcelHospital", con))
+            using (SqlCommand cmd = new SqlCommand("sp_HospitalNamelist", con))
+            {
+                cmd.CommandType = CommandType.StoredProcedure;
+                con.Open();
+                DataTable dt = new DataTable();
+                using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                {
+                    adapter.Fill(dt);
+                }
+                ddlHospitals.DataSource = dt;
+                ddlHospitals.DataTextField = "HospitalName";
+                ddlHospitals.DataValueField = "HospitalId";
+                ddlHospitals.DataBind();
+            }
+        }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+        ddlHospitals.Items.Insert(0, new ListItem("---select---", ""));
+    }
+    protected void btnSearch_Click(object sender, EventArgs e)
+    {
+        lblError.Visible = false; // Hide error label at the beginning of the method
+
+        try
+        {
+
+        }
+        catch (Exception ex)
+        {
+            // Display error message in case of an exception
+            lblError.Text = "An error occurred: " + ex.Message;
+            lblError.Visible = true;
+        }
+        finally
+        {
+            if (con.State == ConnectionState.Open)
+            {
+                con.Close();
+            }
+        }
+    }
+    protected void btnReset_Click(object sender, EventArgs e)
+    {
+        ddlHospitals.SelectedIndex = 0;
+        tbRegisteredFromDate.Text = "";
+        TextBox1.Text = "";
+        lblError.Visible = false;
+    }
+}
